4843b1954e0c0ebb718e7765811b06988c8592f6
usernameiets016 <hensley.troenoredjo@student.rocvf.nl>
2017-02-07 12:02:34 +0100

    Initial commit

--- src/h14/Multimedia2.java
+++ src/h14/Multimedia2.java
@@ -6,58 +6,72 @@ import java.util.*;
 import java.net.*;
 
 public class Multimedia2 extends Applet {
-
     Button deelKaartenKnop = new Button("Deel kaarten");
-    String speler1String = "";
-    String[] kleurenStringArray = {"Schoppen ", "Harten ", "Klaveren ", "Ruiten "},
+    String[] deck = new String[52],
+            kleurenStringArray = {"Schoppen ", "Harten ", "Klaveren ", "Ruiten "},
             kaartenStringArray = {"aas", "twee", "drie", "vier", "vijf", "zes", "zeven", "acht", "negen", "tien", "boer", "vrouw", "heer"},
-            speler1 = new String[12],
-            speler2 = new String[12],
-            speler3 = new String[12],
-            speler4 = new String[12];
+            speler1 = new String[13],
+            speler2 = new String[13],
+            speler3 = new String[13],
+            speler4 = new String[13];
+
+    private String deelKaart() {
+        int random = new Random().nextInt(deck.length);
+        String kaart = deck[random];
+
+        //vervang de inhoud van deck
+        String[] hulpLijst = new String[deck.length - 1];
+        int hulpindex = 0;
+        for (int i = 0; i < deck.length; i++) {
+            if (i != random) {
+                hulpLijst[hulpindex] = deck[i];
+                hulpindex++;
+            }
+        }
+        deck = hulpLijst;
+        return kaart;
+    }
+
     public void init() {
-        deelKaartenKnop.addActionListener(new DeelKaartenKnopListener());
+        deelKaartenKnop.addActionListener(new DeelKaartKnopListener());
         add(deelKaartenKnop);
+        int teller = 0;
+        for (int i = 0; i < kleurenStringArray.length; i++) {
+
+            for (int j = 0; j < kaartenStringArray.length; j++) {
+                if (teller < deck.length) {
+                    deck[teller] = kleurenStringArray[i] + kaartenStringArray[j];
+                    teller++;
+                }
+            }
+        }
+        for (int k = 0; k < speler1.length; k++) {
+            speler1[k] = deelKaart();
+            speler2[k] = deelKaart();
+            speler3[k] = deelKaart();
+            speler4[k] = deelKaart();
+        }
     }
 
     public void paint(Graphics g) {
 
-        for (int teller = 0; teller < kleurenStringArray.length; teller++) {
-            g.drawString(speler1String, 60, 40 * teller  + 20);
-        }
-
     }
 
 
-    class DeelKaartenKnopListener implements ActionListener {
+
+    class DeelKaartKnopListener implements ActionListener {
         public void actionPerformed(ActionEvent e) {
 
-            int randomKaartKleur, randomKaartNummer;
-
-            randomKaartKleur = new Random().nextInt(kleurenStringArray.length);
-            randomKaartNummer = new Random().nextInt(kaartenStringArray.length);
 
 
-            // Tekst op het scherm laten verschijnen
-            speler1String = kleurenStringArray[randomKaartKleur] + kaartenStringArray[randomKaartNummer];
-            repaint();
+            for (int i = 0; i < speler1.length; i++) {
+
+                getGraphics().drawString(speler1[i], 10, 20 * i + 20);
+                getGraphics().drawString(speler2[i], 110, 20 * i + 20);
+                getGraphics().drawString(speler3[i], 210, 20 * i + 20);
+                getGraphics().drawString(speler4[i], 310, 20 * i + 20);
+            }
 
         }
     }
 }
-//    private String deelKaart() {
-//        int random = new Random().nextInt(deck.length);
-//        String kaart = deck[random];
-//
-//        //vervang de inhoud van deck
-//        String[] hulpLijst = new String[deck.length - 1];
-//        int hulpindex = 0;
-//        for (int i = 0; i < deck.length; i++) {
-//            if (i != random) {
-//                hulpLijst[hulpindex] = deck[i];
-//                hulpindex++;
-//            }
-//        }
-//        deck = hulpLijst;
-//        return kaart;
-//    }
