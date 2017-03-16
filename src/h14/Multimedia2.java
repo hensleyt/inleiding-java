192dd80e499a4bebad09bc34467d8099867e7eba
usernameiets016 <hensley.troenoredjo@student.rocvf.nl>
2017-03-16 09:27:46 +0100

    Initial commit

--- src/h14/Multimedia2.java
+++ src/h14/Multimedia2.java
@@ -1,4 +1,5 @@
 package h14;
+
 import java.applet.*;
 import java.awt.*;
 import java.awt.event.*;
@@ -6,7 +7,9 @@ import java.util.*;
 import java.net.*;
 
 public class Multimedia2 extends Applet {
+
     Button deelKaartenKnop = new Button("Deel kaarten");
+
     String[] deck = new String[52],
             kleurenStringArray = {"Schoppen ", "Harten ", "Klaveren ", "Ruiten "},
             kaartenStringArray = {"aas", "twee", "drie", "vier", "vijf", "zes", "zeven", "acht", "negen", "tien", "boer", "vrouw", "heer"},
@@ -33,11 +36,19 @@ public class Multimedia2 extends Applet {
     }
 
     public void init() {
+
+        for (int k = 0; k < speler1.length; k++) {
+            speler1[k] = "";
+            speler2[k] = "";
+            speler3[k] = "";
+            speler4[k] = "";
+        }
+
         deelKaartenKnop.addActionListener(new DeelKaartKnopListener());
         add(deelKaartenKnop);
+
         int teller = 0;
         for (int i = 0; i < kleurenStringArray.length; i++) {
-
             for (int j = 0; j < kaartenStringArray.length; j++) {
                 if (teller < deck.length) {
                     deck[teller] = kleurenStringArray[i] + kaartenStringArray[j];
@@ -45,33 +56,45 @@ public class Multimedia2 extends Applet {
                 }
             }
         }
-        for (int k = 0; k < speler1.length; k++) {
-            speler1[k] = deelKaart();
-            speler2[k] = deelKaart();
-            speler3[k] = deelKaart();
-            speler4[k] = deelKaart();
-        }
     }
 
     public void paint(Graphics g) {
 
+        for (int i = 0; i < speler1.length; i++) {
+            g.drawString(speler1[i], 10, 20 * i + 20);
+            g.drawString(speler2[i], 110, 20 * i + 20);
+            g.drawString(speler3[i], 210, 20 * i + 20);
+            g.drawString(speler4[i], 310, 20 * i + 20);
+        }
     }
-
-
 
     class DeelKaartKnopListener implements ActionListener {
         public void actionPerformed(ActionEvent e) {
 
+            AudioClip deelKaartSound;
+            URL padSound = Multimedia2.class.getResource("/resources/");
+            deelKaartSound = getAudioClip(padSound, "ui_arcade_multiply1.wav");
 
-
-            for (int i = 0; i < speler1.length; i++) {
-
-                getGraphics().drawString(speler1[i], 10, 20 * i + 20);
-                getGraphics().drawString(speler2[i], 110, 20 * i + 20);
-                getGraphics().drawString(speler3[i], 210, 20 * i + 20);
-                getGraphics().drawString(speler4[i], 310, 20 * i + 20);
+            deck = new String[52];
+            int teller = 0;
+            for (int i = 0; i < kleurenStringArray.length; i++) {
+                for (int j = 0; j < kaartenStringArray.length; j++) {
+                    if (teller < deck.length) {
+                        deck[teller] = kleurenStringArray[i] + kaartenStringArray[j];
+                        teller++;
+                    }
+                }
             }
 
+            for (int k = 0; k < speler1.length; k++) {
+                speler1[k] = deelKaart();
+                speler2[k] = deelKaart();
+                speler3[k] = deelKaart();
+                speler4[k] = deelKaart();
+            }
+
+            repaint();
+            deelKaartSound.play();
         }
     }
 }
