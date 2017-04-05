9e6d291e53ab9b38e99c8d2dfd5de326d611d912
usernameiets016 <hensley.troenoredjo@student.rocvf.nl>
2017-04-05 10:43:57 +0200

    Initial commit

--- src/h14/MultimediaPraktijk.java
+++ src/h14/MultimediaPraktijk.java
@@ -1,72 +1,201 @@
+
 package h14;
 import java.applet.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.net.*;
 
 public class MultimediaPraktijk extends Applet {
 
-    int aantalSmiley = 23;
-    Label invoerGetalLabel = new Label("Hoeveel smileys neem je (één, twee of drie)?");
-    TextField invoerGetal = new TextField("", 5);
-    Button knop = new Button("Speel");
-    String aantalSmileyString = "Aantal smileys: " + aantalSmiley + ". Jouw beurt.",
-            invoerCheckString = "",
-            aantalWeggehaaldComputerString = "",
-            aantalWeggehaaldSpelerString = "";
+    private Image aantal_Plaatjes,
+            einde_Plaatje;
+
+    private boolean errorBoolean, spelGestart;
+    private int aantal_Stenen = 23,
+            aantal_Computer_Zet = 0;
+    private Label tekstvakLabel = new Label("Hoeveel smileys neem je (één, twee of drie)?");
+    private TextField tekstvak = new TextField("", 5);
+    private Button startKnop = new Button("Start spel"),
+            speelKnop = new Button("Speel"),
+            nieuwSpelKnop = new Button("Nieuw spel");
+    private String aantal_Plaatjes_Pad = "Stone.png",
+            einde_Plaatje_Pad = "",
+            regel1 = "",
+            regel2 = "",
+            regel3 = "";
+
     public void init() {
 
-        add(invoerGetalLabel);
-        add(invoerGetal);
-        knop.addActionListener(new KnopListener());
-        add(knop);
+        URL pad;
+        pad = MultimediaPraktijk.class.getResource("/resources/");
+        aantal_Plaatjes = getImage(pad, aantal_Plaatjes_Pad);
+        einde_Plaatje = getImage(pad, einde_Plaatje_Pad);
+        // Om het spel te starten
+        startKnop.addActionListener(new StartKnopListener());
+        add(startKnop);
+
+        // Nieuwe Spel Knop wordt pas gebruikt wanneer het spel is geeïndigd.
+        nieuwSpelKnop.addActionListener(new MultimediaPraktijk.NieuwSpelKnopListener());
     }
 
-    public void paint (Graphics g) {
+    public void paint(Graphics g) {
 
-        //Ha!!! Je hebt verloren.
-        //Hmmm... je hebt gewonnen
-        g.drawString(aantalWeggehaaldComputerString, 10, 40);
-        g.drawString(aantalWeggehaaldSpelerString, 10, 60);
-        g.drawString(aantalSmileyString, 10, 80);
-        g.setColor(Color.RED);
-        g.drawString(invoerCheckString, 10, 100);
+        // Als invoer fout is errorBoolean = true
+        if (errorBoolean){
+            g.setColor(Color.RED);
+        }
+        g.drawString(regel1, 10, 40);
+        g.drawString(regel2, 10, 60);
+        g.drawString(regel3, 10, 80);
+        g.drawImage(einde_Plaatje, 95, 60, 200, 200, this);
+
+        // Als spel is gestart, spelGestart = true
+        if (spelGestart) {
+            int x = 10, y = 100;
+            for (int i = 0; i < aantal_Stenen; i++) {
+                if (i == 7 || i == 14 || i == 21) {
+                    x = 10;
+                    y += 35;
+                }
+                g.drawImage(aantal_Plaatjes, x, y, 30, 30, this);
+                x += 35;
+            }
+        }
     }
 
-    class KnopListener implements ActionListener {
-        public void actionPerformed (ActionEvent e){
+    // Toegevoegde knoppen worden zichtbaar
+    private void repaintButtons(){
+        setSize(401, 300);
+        setSize(400, 300);
+    }
 
-            int beurtSpeler = Integer.parseInt(invoerGetal.getText()), beurtComputer = 0;
-            if (beurtSpeler > 0 && beurtSpeler < 4) {
+    // Spel wordt gestart \\
+    class StartKnopListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
 
-                invoerCheckString = "";
-                aantalSmiley = (aantalSmiley - beurtSpeler);
-                aantalSmiley = (aantalSmiley - beurtComputer);
-                aantalSmileyString =  "Aantal smileys: " + aantalSmiley + ". Jouw beurt.";
-                aantalWeggehaaldComputerString = "De computer heeft " + beurtComputer + " smileys weggehaald.";
-                aantalWeggehaaldSpelerString = "Je had daarvoor " + beurtSpeler + " smileys weggehaald.";
+            spelGestart = true;
+            remove(startKnop);
 
-                if (aantalSmiley < 0){
-                    invoerGetal.setEnabled(false);
-                    knop.setEnabled(false);
-                    aantalWeggehaaldComputerString = "";
-                    aantalWeggehaaldSpelerString = "";
-                    if (aantalSmiley - beurtSpeler < 0){
-                        aantalSmileyString =  "Ha!!! Je hebt verloren.";
-                    }
-                    if (aantalSmiley - beurtComputer < 0){
-                        aantalSmileyString =  "Hmmm... je hebt gewonnen";
-                    }
-                    remove(invoerGetal);
-                    remove(knop);
-                    Button nieuwSpelKnop = new Button("Nieuw spel");
-                    nieuwSpelKnop.addActionListener(new KnopListener());
+            add(tekstvakLabel);
+            add(tekstvak);
+
+            // Speel Knop wordt pas gebruikt wanneer er op de Start Knop is gedrukt.
+            speelKnop.addActionListener(new SpeelKnopListener());
+            add(speelKnop);
+
+            repaintButtons();
+        }
+    }
+
+    // Spel Berekeningen \\
+    private void computerZet() {
+        double RandomGetal = Math.random();
+        int random = (int) (RandomGetal * 3 + 1);
+
+        if (aantal_Stenen > 20) {
+            aantal_Computer_Zet = random;
+        } else if (aantal_Stenen % 4 == 0){
+            aantal_Computer_Zet = 3;
+            aantal_Plaatjes_Pad = "Magma.png";
+        } else if (aantal_Stenen % 4 == 2){
+            aantal_Computer_Zet = 1;
+            aantal_Plaatjes_Pad = "Magma.png";
+        } else if (aantal_Stenen % 4 == 3){
+            aantal_Computer_Zet = 2;
+            aantal_Plaatjes_Pad = "Magma.png";
+        } else if (aantal_Stenen % 4 == 1){
+            aantal_Computer_Zet = random;
+        }
+        aantal_Stenen = aantal_Stenen - aantal_Computer_Zet;
+    }
+
+    class SpeelKnopListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+
+            int aantal_Speler_Zet;
+            aantal_Speler_Zet = Integer.parseInt(tekstvak.getText());
+
+            if (aantal_Speler_Zet > 0 && aantal_Speler_Zet < 4){
+
+                if(errorBoolean) {
+                    errorBoolean = false;
+                }
+                aantal_Stenen = aantal_Stenen - aantal_Speler_Zet;
+
+                // Als de computer gewonnen heeft
+                if (aantal_Stenen < 1) {
+
+                    spelGestart = false;
+
+                    regel1 = "Ha!!! Je hebt verloren.";
+                    regel2 = "";
+                    regel3 = "";
+
+                    remove(tekstvakLabel);
+                    remove(tekstvak);
+                    remove(speelKnop);
+
                     add(nieuwSpelKnop);
+
+                    repaintButtons();
+
+                    einde_Plaatje_Pad = "haha_face.png";
+                // Als de speler gewonnen heeft
+                } else if (aantal_Stenen == 1) {
+
+                    spelGestart = false;
+
+                    regel1 = "Hmmm... je hebt gewonnen";
+                    regel2 = "";
+                    regel3 = "";
+
+                    remove(tekstvakLabel);
+                    remove(tekstvak);
+                    remove(speelKnop);
+
+                    add(nieuwSpelKnop);
+
+                    repaintButtons();
+
+                    einde_Plaatje_Pad = "winner-trophy.png";
+                } else {
+                    computerZet();
+                    regel1 = "De computer heeft " + aantal_Computer_Zet + " stenen weggehaald.";
+                    regel2 = "Aantal stenen: " + aantal_Stenen + ". Jouw beurt.";
+                    regel3 = "Je had hiervoor " + aantal_Speler_Zet + " stenen weggehaald.";
                 }
             }
+            // Als invoer fout is
             else {
+                errorBoolean = true;
+                regel1 = "Onjuiste invoer";
+                regel2 = "";
+                regel3 = "";
+                // if TextField is empty
+                // regel1 = "Voer een geheel getal in";
+           }
+            repaint();
+        }
+    }
 
-                invoerCheckString = "Onjuiste invoer";
-            }
+    // Nieuwe Spel \\
+    class NieuwSpelKnopListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+
+            spelGestart = true;
+
+            regel1 = "";
+            regel2 = "";
+            regel3 = "";
+
+            aantal_Stenen = 23;
+
+            add(tekstvakLabel);
+            add(tekstvak);
+            add(speelKnop);
+            remove(nieuwSpelKnop);
+
+            repaintButtons();
             repaint();
         }
     }
